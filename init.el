d13dee03253f0388d1b5e1316e94caaf24744742
ichi-t <taisuke_ichimura@icloud.com>
2022-08-01 21:18:03 +0900

    update init.el

---
+++ init.el
@@ -0,0 +1,262 @@
+;;; init.el --- My init.el  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;; My init.el.
+
+;;; Code:
+
+;; this enables this running method
+
+(eval-and-compile
+  (when (or load-file-name byte-compile-current-file)
+    (setq user-emacs-directory
+          (expand-file-name
+           (file-name-directory (or load-file-name byte-compile-current-file))))))
+
+(eval-and-compile
+  (customize-set-variable
+   'package-archives '(("gnu"   . "https://elpa.gnu.org/packages/")
+                       ("melpa" . "https://melpa.org/packages/")
+                       ("org"   . "https://orgmode.org/elpa/")))
+  (package-initialize)
+  (unless (package-installed-p 'leaf)
+    (package-refresh-contents)
+    (package-install 'leaf))
+
+  (leaf leaf-keywords
+    :ensure t
+    :init
+    ;; optional packages if you want to use :hydra, :el-get, :blackout,,,
+    (leaf hydra :ensure t)
+    (leaf el-get :ensure t)
+    (leaf blackout :ensure t)
+
+    :config
+    ;; initialize leaf-keywords.el
+    (leaf-keywords-init)))
+
+;; ここにいっぱい設定を書く
+
+(leaf key-settings
+  :doc "key settings"
+  :config
+  (keyboard-translate ?\C-h ?\C-?)
+  (global-unset-key "\C-t"))
+
+(leaf cus-edit
+  :doc "tools for customizing Emacs and Lisp packages"
+  :tag "builtin" "faces" "help"
+  :custom `((custom-file . ,(locate-user-emacs-file "custom.el"))))
+
+(leaf cus-start
+  :doc "define customization properties of builtins"
+  :tag "builtin" "internal"
+  :preface
+  (defun c/redraw-frame nil
+    (interactive)
+    (redraw-frame))
+
+  :bind (("M-ESC ESC" . c/redraw-frame))
+  :custom '((user-full-name . "Taisuke Ichimura")
+            (user-mail-address . "taisuke_ichimura@icloud.com")
+            (user-login-name . "ichi-t")
+            (create-lockfiles . nil)
+            (debug-on-error . t)
+            (init-file-debug . t)
+            (frame-resize-pixelwise . t)
+            (enable-recursive-minibuffers . t)
+            (history-length . 1000)
+            (history-delete-duplicates . t)
+            (scroll-preserve-screen-position . t)
+            (scroll-conservatively . 100)
+            (mouse-wheel-scroll-amount . '(1 ((control) . 5)))
+            (ring-bell-function . 'ignore)
+            (text-quoting-style . 'straight)
+            (truncate-lines . t)
+            ;; (use-dialog-box . nil)
+            ;; (use-file-dialog . nil)
+            (menu-bar-mode . nil)
+            (tool-bar-mode . nil)
+            (scroll-bar-mode . nil)
+            (indent-tabs-mode . nil))
+  :config
+  (defalias 'yes-or-no-p 'y-or-n-p)
+  (keyboard-translate ?\C-h ?\C-?)
+  (electric-indent-mode 0))
+
+(leaf multiple-cursors :ensure t)
+
+(leaf smartrep
+  :ensure t
+  :config
+  (smartrep-define-key global-map "C-t"
+    '(("C-t"      . 'mc/mark-next-like-this)
+      ("n"        . 'mc/mark-next-like-this)
+      ("p"        . 'mc/mark-previous-like-this)
+      ("m"        . 'mc/mark-more-like-this-extended)
+      ("u"        . 'mc/unmark-next-like-this)
+      ("U"        . 'mc/unmark-previous-like-this)
+      ("s"        . 'mc/skip-to-next-like-this)
+      ("S"        . 'mc/skip-to-previous-like-this)
+      ("*"        . 'mc/mark-all-like-this)
+      ("d"        . 'mc/mark-all-like-this-dwim)
+      ("i"        . 'mc/insert-numbers)
+      ("o"        . 'mc/sort-regions)
+      ("O"        . 'mc/reverse-regions))))
+
+(leaf exec-path-from-shell
+  :ensure t
+  :defun (exec-path-from-shell-initialize)
+  :custom
+  ((exec-path-from-shell-check-startup-files . nil)
+   (exec-path-from-shell-arguments . nil)
+   (exec-path-from-shell-variables
+    . '(
+        "DEBEMAIL"
+        "DEBFULLNAME"
+        "GPG_AGENT_INFO"
+        "GPG_KEY_ID"
+        "PASSWORD_STORE_DIR"
+        "PATH"
+        "SHELL"
+        "SKKSERVER"
+        "TEXMFHOME"
+        "WSL_DISTRO_NAME"
+        "http_proxy"
+        )))
+  :config
+  (exec-path-from-shell-initialize)
+  (setq user-full-name    (concat (getenv "DEBFULLNAME"))
+        user-mail-address (concat (getenv "DEBEMAIL")))
+  (defconst my:d:password-store
+    (if (getenv "PASSWORD_STORE_DIR")
+        (expand-file-name (concat "Emacs/" (system-name))
+                          (getenv "PASSWORD_STORE_DIR")) ""))
+  )
+
+(leaf autorevert
+  :doc "revert buffers when files on disk change"
+  :tag "builtin"
+  :custom ((auto-revert-interval . 1))
+  :global-minor-mode global-auto-revert-mode)
+
+(leaf paren
+  :doc "highlight matching paren"
+  :tag "builtin"
+  :custom ((show-paren-delay . 0.1))
+  :global-minor-mode show-paren-mode)
+
+(leaf ivy
+  :doc "Incremental Vertical completYon"
+  :req "emacs-24.5"
+  :tag "matching" "emacs>=24.5"
+  :url "https://github.com/abo-abo/swiper"
+  :emacs>= 24.5
+  :ensure t
+  :blackout t
+  :leaf-defer nil
+  :custom ((ivy-initial-inputs-alist . nil)
+           (ivy-use-selectable-prompt . t))
+  :global-minor-mode t
+  :config
+  (leaf swiper
+    :doc "Isearch with an overview. Oh, man!"
+    :req "emacs-24.5" "ivy-0.13.0"
+    :tag "matching" "emacs>=24.5"
+    :url "https://github.com/abo-abo/swiper"
+    :emacs>= 24.5
+    :ensure t
+    :bind (("C-s" . swiper)))
+
+  (leaf counsel
+    :doc "Various completion functions using Ivy"
+    :req "emacs-24.5" "swiper-0.13.0"
+    :tag "tools" "matching" "convenience" "emacs>=24.5"
+    :url "https://github.com/abo-abo/swiper"
+    :emacs>= 24.5
+    :ensure t
+    :blackout t
+    :bind (("C-S-s" . counsel-imenu)
+           ("C-x C-r" . counsel-recentf))
+    :custom `((counsel-yank-pop-separator . "\n----------\n")
+              (counsel-find-file-ignore-regexp . ,(rx-to-string '(or "./" "../") 'no-group)))
+    :global-minor-mode t))
+
+(leaf prescient
+  :doc "Better sorting and filtering"
+  :req "emacs-25.1"
+  :tag "extensions" "emacs>=25.1"
+  :url "https://github.com/raxod502/prescient.el"
+  :emacs>= 25.1
+  :ensure t
+  :custom ((prescient-aggressive-file-save . t))
+  :global-minor-mode prescient-persist-mode)
+  
+(leaf ivy-prescient
+  :doc "prescient.el + Ivy"
+  :req "emacs-25.1" "prescient-4.0" "ivy-0.11.0"
+  :tag "extensions" "emacs>=25.1"
+  :url "https://github.com/raxod502/prescient.el"
+  :emacs>= 25.1
+  :ensure t
+  :after prescient ivy
+  :custom ((ivy-prescient-retain-classic-highlighting . t))
+  :global-minor-mode t)
+
+(leaf flycheck
+  :doc "On-the-fly syntax checking"
+  :req "dash-2.12.1" "pkg-info-0.4" "let-alist-1.0.4" "seq-1.11" "emacs-24.3"
+  :tag "minor-mode" "tools" "languages" "convenience" "emacs>=24.3"
+  :url "http://www.flycheck.org"
+  :emacs>= 24.3
+  :ensure t
+  :bind (("M-n" . flycheck-next-error)
+         ("M-p" . flycheck-previous-error))
+  :global-minor-mode global-flycheck-mode)
+
+(leaf company
+  :doc "Modular text completion framework"
+  :req "emacs-24.3"
+  :tag "matching" "convenience" "abbrev" "emacs>=24.3"
+  :url "http://company-mode.github.io/"
+  :emacs>= 24.3
+  :ensure t
+  :blackout t
+  :leaf-defer nil
+  :bind ((company-active-map
+          ("M-n" . nil)
+          ("M-p" . nil)
+          ("C-s" . company-filter-candidates)
+          ("C-n" . company-select-next)
+          ("C-p" . company-select-previous)
+          ("<tab>" . company-complete-selection))
+         (company-search-map
+          ("C-n" . company-select-next)
+          ("C-p" . company-select-previous)))
+  :custom ((company-idle-delay . 0)
+           (company-minimum-prefix-length . 1)
+           (company-transformers . '(company-sort-by-occurrence)))
+  :global-minor-mode global-company-mode)
+
+(leaf company-c-headers
+  :doc "Company mode backend for C/C++ header files"
+  :req "emacs-24.1" "company-0.8"
+  :tag "company" "development" "emacs>=24.1"
+  :added "2020-03-25"
+  :emacs>= 24.1
+  :ensure t
+  :after company
+  :defvar company-backends
+  :config
+  (add-to-list 'company-backends 'company-c-headers))
+
+;; TODO lsp-mode
+
+(provide 'init)
+
+;; Local Variables:
+;; indent-tabs-mode: nil
+;; End:
+
+;;; init.el ends here
